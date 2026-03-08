e73494cc519a004177d1a05bddeda846a2ed3e5d
akhil-2kb2 <rudraaraju@gmail.com>
2026-03-09 01:49:33 +0530

    first commit

---
+++ src/main/java/com/ppsulife/faculty_life_api/FacultyLifeApiApplication.java
@@ -0,0 +1,13 @@
+package com.ppsulife.faculty_life_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyLifeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyLifeApiApplication.class, args);
+	}
+
+}
